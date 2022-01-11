3773cced33bc3aaf0645f4b8d1fd733a6237952f
doms99 <dominik.mandic@fer.hr>
2022-01-11 22:07:46 +0100

    revert to old score board

--- src/components/game/controllers/RoundPoints.tsx
+++ src/components/game/controllers/RoundPoints.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Round, RoundActions } from '../../../interfaces';
 import Dots from '../../icons/Dots';
+import OptionsMenu from '../views/OptionsMenu';
 import TeamRoundPoints from '../views/TeamRoundPoints';
 
 export interface Props {
@@ -13,18 +14,6 @@ export interface Props {
 
 const RoundPoints: React.FC<Props> = ({ round, teams, onClickActions, selected, index }) => {
   const [clicked, setClicked] = useState<boolean>(false);
-  const rowRef = useRef<HTMLDivElement>(null);
-
-  const setMinWidth = () => {
-    if(rowRef.current === null) {
-      setTimeout(setMinWidth, 0);
-      return;
-    }
-
-    rowRef.current!.style.minWidth = rowRef.current!.style.width;
-  }
-
-  setTimeout(setMinWidth, 0);
 
   const wrapCallback = (callback: (index: number) => void) => {
     return () => {
@@ -49,34 +38,23 @@ const RoundPoints: React.FC<Props> = ({ round, teams, onClickActions, selected, 
 
   return (
     <div 
-      className={`group relative px-5 border-primary
-      ${selected ? "text-black" : "text-gray-400"}
+      className={`group relative grid grid-cols-${teams.length}
+      px-5 border-primary ${selected ? "text-black" : "text-gray-400"}
       hover:bg-gray-100 bg-transparent text-xl font-medium`}
     > 
-      {clicked && (
-        <>
-          <span className="bg-yellow-500">E</span>
-          <span className="bg-blue-400">R</span>
-          <span className="bg-red-500">D</span>
-        </>
-      )}
       <button 
         onClick={click}
         className="hidden group-hover:block active:bg-gray-200 absolute left-0 ml-2 h-full px-2 rounded-md" >
         <Dots/>
       </button>
-      <div ref={rowRef} className="inline-block min-w-full">
-        <div className={`grid grid-cols-${teams.length}`}>
-          {teams.map(team => (
-            <TeamRoundPoints
-              key={team}
-              points={round[team].points}
-              declarations={round[team].declarations}
-            />
-          ))}
-        </div>
-      </div>
-      {/* {clicked && 
+      {teams.map(team => (
+        <TeamRoundPoints 
+          key={team} 
+          points={round[team].points} 
+          declarations={round[team].declarations} 
+        />
+      ))}
+      {clicked && 
         <div className="absolute left-0 bottom-0 ml-2 transform translate-y-full z-50">
           <OptionsMenu
             onClickOptions={onClickActions?.map(
@@ -84,7 +62,7 @@ const RoundPoints: React.FC<Props> = ({ round, teams, onClickActions, selected, 
             )}
           />
         </div>
-      } */}
+      }
     </div>
   );
 };
